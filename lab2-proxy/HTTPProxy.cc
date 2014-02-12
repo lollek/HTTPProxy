a5ce2de3f2b184183cecc0a90ad6ba34c9f719d8
Olle K <olle.kvarnstroem@gmail.com>
2014-02-12 18:40:09 +0100

    test

--- lab2-proxy/HTTPProxy.cc
+++ lab2-proxy/HTTPProxy.cc
@@ -220,7 +220,7 @@ void HTTPProxy::removeKeepAlive(vector<char> &data) const {
     if (!strncmp(datadata + i, open1, strlen(open1)) ||
         !strncmp(datadata + i, open2, strlen(open2))) {
       i += 12;
-      memcpy(datadata + i, "Close", 5);
+      memcpy(datadata + i, "close", 5);
       memcpy(datadata + i + 5, datadata + i + 11, data.size() - i - 5);
       data.resize(data.size() - 5);
       //data.erase(data.begin() +i +5, data.begin() +i +11);
