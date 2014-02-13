40010dbf7278555207253267b220d7f6d93aff5e
Olle K <olle.kvarnstroem@gmail.com>
2014-02-13 12:18:40 +0100

    changed TCPSocket form link to real files

--- lab2-proxy/TCPSocket.cc
+++ lab2-proxy/TCPSocket.cc
@@ -1,1 +1,207 @@
-TCPSocket/TCPSocket.cc
+#include <unistd.h>
+#include <string.h>
+
+#include <string>
+#include <iostream>
+
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define __TCPSOCKET_HIDDEN_FROM_SOURCE__
+#include "TCPSocket.hh"
+
+using namespace std;
+
+TCPSocket::TCPSocket(IPV ip_version) :
+  ip_version_(ip_version),
+  sock_(-1),
+  ip_(new char[INET6_ADDRSTRLEN]),
+  reuseaddr_(false)
+{
+  memset(ip_, 0, INET6_ADDRSTRLEN);
+}
+
+TCPSocket::TCPSocket(const TCPSocket &other) :
+  ip_version_(other.ip_version_),
+  sock_(other.sock_),
+  ip_(new char[INET6_ADDRSTRLEN]),
+  reuseaddr_(other.reuseaddr_)
+{
+  strncpy(ip_, other.ip_, INET6_ADDRSTRLEN);
+  ip_[INET6_ADDRSTRLEN -1] = '\0';
+}
+
+TCPSocket::~TCPSocket() {
+  delete[] ip_;
+  ip_ = NULL;
+}
+
+void TCPSocket::reuseAddr(bool var) {
+  reuseaddr_ = var;
+}
+
+TCPSocket::TCPSocket(int sock, const char *ip, IPV ip_version) :
+  ip_version_(ip_version),
+  sock_(sock),
+  ip_(new char[INET6_ADDRSTRLEN]),
+  reuseaddr_(false)
+{
+  strncpy(ip_, ip, INET6_ADDRSTRLEN);
+  ip_[INET6_ADDRSTRLEN -1] = '\0';
+}
+
+int TCPSocket::construct(const char *hostname, const char *port) {
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = ip_version_ == IPV4 ? AF_INET : AF_INET6;
+  hints.ai_socktype = SOCK_STREAM;
+  if (hostname == NULL) {
+    hints.ai_flags = AI_PASSIVE;
+  }
+
+  struct addrinfo *results = NULL;
+  int status = getaddrinfo(hostname, port, &hints, &results);
+  if (status != 0) {
+    cerr << "Error while looking up address info: "
+         << gai_strerror(status) << '\n';
+    return 1;
+  }
+
+  for (struct addrinfo *p = results; p != NULL; p = p->ai_next) {
+    sock_ = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    if (sock_ == -1) {
+      perror("socket:");
+      continue;
+    }
+
+    if (hostname == NULL) {
+      if (reuseaddr_) {
+        int one = 1;
+        if (setsockopt(sock_, SOL_SOCKET, SO_REUSEADDR,
+                       &one, sizeof(one)) == -1) {
+          perror("setsockopt");
+        }
+      }
+
+      if (bind(sock_, p->ai_addr, p->ai_addrlen) == -1) {
+        close(sock_);
+        perror("bind");
+        sock_ = -1;
+      } else {
+        break;
+      }
+
+    } else {
+      if (connect(sock_, p->ai_addr, p->ai_addrlen) == -1) {
+        close(sock_);
+        perror("connect");
+        sock_ = -1;
+      } else {
+        break;
+      }
+    }
+  }
+
+  freeaddrinfo(results);
+
+  return sock_ == -1;
+}
+
+int TCPSocket::_connect(const string &hostname, int port) {
+  strncpy(ip_, hostname.c_str(), INET6_ADDRSTRLEN);
+  ip_[INET6_ADDRSTRLEN -1] = '\0';
+  return construct(hostname.c_str(), to_string(port).c_str());
+}
+
+int TCPSocket::_bind(int port) {
+  return construct(NULL, to_string(port).c_str());
+}
+
+int TCPSocket::_listen(int num) const {
+  if (listen(sock_, num) == -1) {
+    perror("listen");
+    return 1;
+  }
+  return 0;
+}
+
+TCPSocket *TCPSocket::_accept() const {
+  struct sockaddr_storage client_addr;
+  socklen_t socklen = sizeof(client_addr);
+  int client = accept(sock_, (struct sockaddr *)&client_addr, &socklen);
+  if (client == -1) {
+    return NULL;
+  }
+
+  void *sin_addr = NULL;
+  if (((struct sockaddr *)&client_addr)->sa_family == AF_INET) {
+    sin_addr = &(((struct sockaddr_in *)&client_addr)->sin_addr);
+  } else {
+    sin_addr = &(((struct sockaddr_in6 *)&client_addr)->sin6_addr);
+  }
+  char client_ip[INET6_ADDRSTRLEN];
+  inet_ntop(client_addr.ss_family, sin_addr, client_ip, INET6_ADDRSTRLEN);
+
+  return new TCPSocket(client, client_ip, ip_version_);
+}
+
+vector<char> TCPSocket::_recv(int num) const {
+  vector<char> data(num);
+  int datalen = recv(sock_, data.data(), num, 0);
+  if (datalen == -1) {
+    cerr << "Failed to read from client\n";
+    return vector<char>(0);
+  } else {
+    data.resize(datalen);
+    return data;
+  }
+}
+
+vector<char> TCPSocket::recvall() const {
+  const unsigned BUFSIZE = 1024;
+  vector<char> data(BUFSIZE);
+  int datalen = recv(sock_, data.data(), BUFSIZE, 0);
+  if (datalen == -1 ) {
+    cerr << "Failed to read from client\n";
+    return vector<char>(0);
+  } else {
+    data.resize(datalen);
+
+    if (datalen == BUFSIZE) {
+      vector<char> tmp(BUFSIZE);
+      while ((datalen = recv(sock_, tmp.data(), BUFSIZE, 0)) > 0) {
+        for (int i = 0; i < datalen; ++i) {
+          data.push_back(tmp[i]);
+        }
+      }
+      for (int i = 0; i < datalen; ++i) {
+        data.push_back(tmp[i]);
+      }
+    }
+    return data;
+  }
+}
+
+int TCPSocket::_send(const vector<char> &message) const {
+  const char *data = message.data();
+  int data_left = message.size();
+  while (data_left > 0) {
+    int tmp = send(sock_, data +message.size() -data_left, data_left, 0);
+    if (tmp == -1) {
+      cerr << "Failed to send!\n";
+      return 1;
+    }
+    data_left -= tmp;
+  }
+  return 0;
+}
+
+void TCPSocket::_close() {
+  close(sock_);
+  delete[] ip_;
+  ip_ = NULL;
+}
+
+string TCPSocket::getHostname() const {
+  return string(ip_);
+}
--- lab2-proxy/TCPSocket.hh
+++ lab2-proxy/TCPSocket.hh
@@ -1,1 +1,99 @@
-TCPSocket/TCPSocket.hh
+
+#include <string>
+#include <vector>
+
+enum IPV { IPV4, IPV6 };
+
+class TCPSocket {
+  public:
+    TCPSocket (IPV ip_version);
+    TCPSocket (const TCPSocket &other);
+    ~TCPSocket();
+
+#ifndef __TCPSOCKET_HIDDEN_FROM_SOURCE__
+    /* Connects to remote host
+     * hostname = hostname or ip of remote host
+     * port     = port of remote host to connect to
+     * returns 0 on success
+     */
+    int connect(const std::string &hostname, int port)
+      { return _connect(hostname, port); }
+
+    /* Bind to a local port
+     * port = local port to bind to
+     * returns 0 on success
+     */
+    int bind(int port)
+      { return _bind(port); }
+
+    /* Listen to incoming connections
+     * num = how many incoming connections that can be queued
+     * returns 0 on success
+     */
+    int listen(int num) const
+      { return _listen(num); }
+
+    /* Accept an incoming connection
+     * returns a pointer to the client socket, which must later be deleted
+     */
+    TCPSocket *accept() const
+      { return _accept(); }
+
+    /* Receive data from socket
+     * num = size of buffer
+     * returns a string with received data,
+     *  which is empty if nothing is received
+     */
+    std::vector<char> recv(int num) const
+      { return _recv(num); }
+
+    /* Send data to socket
+     * message = data to send
+     * returns 0 on success
+     */
+    int send(const std::vector<char> &message) const
+      { return _send(message); }
+
+    /* Close socket */
+    void close()
+      { return _close(); }
+#endif
+
+    /* Receive all data from socket
+     * This is basically calling recv() several times */
+    std::vector<char> recvall() const;
+
+    /* Set if socket should reuse port or not
+     * Must be set before bind to have an effect 
+     */
+    void reuseAddr(bool var);
+
+    /* Get hostname of which the socket is connected to */
+    std::string getHostname() const;
+
+    /* Forbidden methods */
+    void operator=(const TCPSocket&) = delete;
+
+  private:
+
+    /* Server */
+    int _bind(int port);
+    int _listen(int num) const;
+    TCPSocket *_accept() const;
+
+    /* Client */
+    int _connect(const std::string &hostname, int port);
+
+    /* Both */
+    TCPSocket(int sock, const char *ip, IPV ip_version);
+    int construct(const char *hostname, const char *port);
+    int _send(const std::vector<char> &message) const;
+    std::vector<char> _recv(int num) const;
+    void _close();
+
+    /* Member variables */
+    const IPV ip_version_;
+    int sock_;
+    char *ip_;
+    bool reuseaddr_;
+};
